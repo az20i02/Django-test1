099af42eb21b3b2c551c21c77642cecca7c7b5d9
Aziz <azoo.new1@gmail.com>
2024-09-20 14:26:20 +0300

    Remove test_file.txt

--- weby/urls.py
+++ weby/urls.py
@@ -3,20 +3,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='weby/index/')),  # Redirect root to weby/index/
     path('admin/', admin.site.urls),
     path('weby/', include('main.urls')),
     path('items/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('inbox/', include('conversation.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 
